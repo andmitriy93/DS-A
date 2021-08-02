7532b8e223e55632aa65da348d95db47190aa1af
Dmitrii An <an.dmitriy93@gmail.com>
2021-08-02 16:15:45 -0500

    monotonic array medium lvl

--- medium/monotonic_arr.js
+++ medium/monotonic_arr.js
@@ -16,3 +16,35 @@
   true
 */
 
+// Time O(n) / Space O(1)
+function isMonotonic(array) {
+  let isNonDecreasing = true;
+  let isNonIncreasing = true;
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < array[i - 1]) isNonDecreasing = false;
+    if (array[i] > array[i - 1]) isNonIncreasing = true;
+  }
+  return isNonDecreasing || isNonIncreasing
+}
+
+// Time O(n) / Space O(1)
+function isMonotonic(array) {
+  if (array.length <= 2) return true;
+
+  let direction = array[1] - array[0];
+  for (let i = 2; i < array.length; i++) {
+    if (direction === 0) {
+      direction = array[i] - array[i - 1]
+      continue;
+    }
+    if (breakDirection(direction, array[i - 1], array[i])) return false
+  }
+  return true;
+}
+
+function breakDirection(direction, prevNum, currentNum) {
+  const difference = currentNum - prevNum;
+  if (direction > 0) return difference < 0
+  return difference > 0
+}
