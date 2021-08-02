40918125d465a58fc8024cf490c06040b00a038e
Dmitrii An <an.dmitriy93@gmail.com>
2021-08-02 15:09:06 -0500

    Solve move element to end problem medium lvl

--- medium/move_element.js
+++ medium/move_element.js
@@ -14,3 +14,42 @@
   [1, 3, 4, 2, 2, 2, 2, 2]
 */
 
+// Time O(nlogn) / Space O(1)
+function moveElementToEnd(array, toMove) {
+  array.sort((a, b) => a - b)
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start < end) {
+    if (array[start] === toMove && array[end] !== toMove) {
+      swap(start, end, array);
+      start++;
+      end--;
+    } else if (array[start] === toMove && array[end] === toMove) {
+      end--;
+    } else if (array[start] !== toMove && array[end] === toMove) {
+      end--;
+    } else {
+      start++;
+    }
+  }
+  return array;
+}
+
+function swap(i, j, array) {
+  let tmp = array[j];
+  array[j] = array[i];
+  array[i] = tmp;
+}
+
+// Time O(n) / Space O(1)
+function moveElementToEnd(array, toMove) {
+  let i = 0;
+  let j = array.length - 1;
+  while (i < j) {
+    while (i < j && array[j] === toMove) j--;
+    if (array[i] === toMove) swap(i, j, array);
+    i++;
+  }
+  return array;
+}
