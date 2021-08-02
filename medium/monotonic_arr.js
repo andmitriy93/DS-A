7709c7e3ca1f2c6d5efed081c7e73c911f94d0eb
Dmitrii An <an.dmitriy93@gmail.com>
2021-08-02 15:25:55 -0500

    Create monotinic array problem

---
+++ medium/monotonic_arr.js
@@ -0,0 +1,18 @@
+/* 
+  Write a function that takes in an array of integers and returns a boolean representing whter the array 
+  is monotonic.
+
+  An array is said to be monotinic if its elements, from left to right, are entirely non-incereasing or 
+  entirely non-decresing.
+
+  Non-increasing elements aren't necessarily exlusively decreasing: they simply don't increase. Similarly,
+  non-decreasing elements aren't necessarily exclusively increasing: they simply don't decrease.
+
+  Note that empty arrays and arrays of one element are monotonic.
+
+  INPUT:
+  array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+  OUTPUT:
+  true
+*/
+
