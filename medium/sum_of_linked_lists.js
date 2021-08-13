cb8b165e3ca8e89ccc4282ba6718c75307363503
Dmitrii An <an.dmitriy93@gmail.com>
2021-08-13 10:50:00 -0500

    Solved Sub of linked lists medium lvl AlgoExpert

--- medium/sum_of_linked_lists.js
+++ medium/sum_of_linked_lists.js
@@ -21,3 +21,36 @@
   1742 + 549 = 2291
 
 */
+
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function sumofLinkedLists(linkedListOne, linkedListTwo) {
+  const newLinkedListPointer = new LinkedList(0)
+  let currentNode = newLinkedListPointer;
+  let carry = 0;
+
+  let nodeOne = linkedListOne;
+  let nodeTwo = linkedListTwo;
+
+  while (nodeOne !== null || nodeTwo !== null || carry !== 0) {
+    let valueOne = nodeOne !== null ? nodeOne.value : 0;
+    let valueTwo = nodeTwo !== null ? nodeTwo.value : 0;
+    let sumValues = valueOne + valueTwo + carry;
+
+    let newValue = sumValues % 10;
+    let newNode = new LinkedList(newValue);
+    currentNode.next = newNode;
+    currentNode = newNode;
+
+    carry = Math.floor(sumValues / 10);
+    nodeOne = nodeOne !== null ? nodeOne.next : null
+    nodeTwo = nodeTwo !== null ? nodeTwo.next : null
+  }
+
+  return newLinkedListPointer.next
+}
