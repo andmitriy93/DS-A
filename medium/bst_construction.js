3147367dc18fff2a2823af452c633a3307e739f7
Dmitrii An <an.dmitriy93@gmail.com>
2021-12-07 19:26:57 -0600

    Recursively and iteratively insert and contains methods

--- medium/bst_construction.js
+++ medium/bst_construction.js
@@ -30,17 +30,80 @@ class BST {
     this.right = null;
   }
 
+  // Recursively
   insert(value) {
-
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+    return this;
   }
 
+  // Iteratively
+  insert(value) {
+    let currentNode = this;
+    while (true) {
+      if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
+    return this;
+  }
+
+  // Recursively
   contains(value) {
-
+    if (value < this.value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value)
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        return false
+      } else {
+        return this.right.contains(value)
+      }
+    } else {
+      return true;
+    }
   }
 
+  // Iteratively
+  contains(value) {
+    let currentNode = this;
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 
   remove(value) {}
-
-
-
 }
