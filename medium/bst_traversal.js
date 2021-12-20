e365d3e37e0bcce066ae0579b9b734bc09224f63
Dmitrii An <an.dmitriy93@gmail.com>
2021-12-20 16:36:01 -0600

    BSt traverse inOrderTraverse

--- medium/bst_traversal.js
+++ medium/bst_traversal.js
@@ -22,7 +22,14 @@
 
 */
 
-function inOrderTraverse(tree, array) {}
+function inOrderTraverse(tree, array) {
+  if (tree !== null) {
+    inOrderTraverse(tree.left, array)
+    array.push(tree.value)
+    inOrderTraverse(tree.right, array)
+  }
+  return array;
+}
 
 function preOrderTraverse(tree, array) {}
 
