bec9c645164463a92551e6eb4befd35f77d63eeb
Dmitrii An <an.dmitriy93@gmail.com>
2021-12-20 16:43:39 -0600

    BST traverse postOrderTraverse. Finish with bst traverse

--- medium/bst_traversal.js
+++ medium/bst_traversal.js
@@ -40,4 +40,11 @@ function preOrderTraverse(tree, array) {
   return array
 }
 
-function postOrderTraverse(tree, array) {}
+function postOrderTraverse(tree, array) {
+  if (tree !== null) {
+    postOrderTraverse(tree.left, array)
+    postOrderTraverse(tree.right, array)
+    array.push(tree.value)
+  }
+  return array
+}
