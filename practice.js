6d0b7e14667bba4afb0a99dc039f73a6c787f9d7
Dmitrii An <an.dmitriy93@gmail.com>
2022-01-05 13:15:14 -0600

    Merge sort

--- practice.js
+++ practice.js
@@ -12,18 +12,15 @@ function removeDuplicatesFromLinkedList(linkedList) {
 
   while (currentNode.next !== null) {
     if (currentNode.value === currentNode.next.value) {
-      currentNode.next = currentNode.next.next
+      currentNode.next = currentNode.next.next;
     } else {
-			currentNode = currentNode.next	
-		}
+      currentNode = currentNode.next;
+    }
   }
-  return linkedList
+  return linkedList;
 }
 
 // // ll = 1 -> 1 -> 3 -> 4 -> 4 -> 4 -> 5 -> 6 -> 6
-
-
-
 
 // Merge two linked lists
 
@@ -35,28 +32,27 @@ class LinkedList {
 }
 
 function mergeLinkedLists(headOne, headTwo) {
-	let p1 = headOne
-  let p2 = headTwo
+  let p1 = headOne;
+  let p2 = headTwo;
   let prev = null;
 
   while (p1 && p2) {
     if (p1.value < p2.value) {
       prev = p1;
-      p1 = p1.next
+      p1 = p1.next;
     } else {
-      if (prev !== null) prev.next = p2
+      if (prev !== null) prev.next = p2;
       prev = p2;
       p2 = p2.next;
-      prev.next = p1
+      prev.next = p1;
     }
   }
   if (p1 === null) prev.next = p2;
 
-  return headOne.value < headTwo.value ? headOne : headTwo
+  return headOne.value < headTwo.value ? headOne : headTwo;
 }
 
-
-// Reverse linked list 
+// Reverse linked list
 
 function reverseLinkedList(head) {
   let currentNode = head;
@@ -65,13 +61,12 @@ function reverseLinkedList(head) {
   while (currentNode) {
     let tmp = currentNode.next;
     currentNode.next = previous;
-    previous = currentNode
+    previous = currentNode;
     currentNode = tmp;
   }
 
   return previous;
 }
-
 
 // Bubble sort
 
@@ -82,17 +77,74 @@ function bobbleSort(array) {
     isSorted = true;
     for (let idx = 0; idx < array.length; idx++) {
       if (array[idx] > array[idx + 1]) {
-        swap(idx, idx + 1, array)
-        isSorted = false
+        swap(idx, idx + 1, array);
+        isSorted = false;
       }
     }
   }
 
-  return array
+  return array;
 }
 
 function swap(idx, idx2, array) {
-  let tmp = array[idx2]
+  let tmp = array[idx2];
   array[idx2] = array[idx];
-  array[idx] = tmp
+  array[idx] = tmp;
+}
+
+// Quicksort
+
+function quickSort(array) {
+  if (array.length <= 1) return array;
+
+  const pivot = array[array.length - 1];
+  const leftArray = [];
+  const rightArray = [];
+
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] < pivot) {
+      leftArray.push(array[i]);
+    } else {
+      rightArray.push(array[i]);
+    }
+  }
+
+  return [...quickSort(leftArray), pivot, ...quickSort(rightArray)];
+}
+
+// Merge sort
+
+function mergeSort(array) {
+  if (array.length <= 1) return array;
+
+  const midIdx = Math.floor(array.length / 2);
+  const leftHalf = array.slice(0, midIdx);
+  const rightHalf = array.slice(midIdx);
+
+  return mergeHelper(mergeSort(leftHalf), mergeSort(rightHalf));
+}
+
+function mergeHelper(leftHalf, rightHalf) {
+  let result = new Array(leftHalf.length + rightHalf.length)
+  let k = 0;
+  let j = 0;
+  let i = 0;
+
+  while (i < leftHalf.length && j < rightHalf.length) {
+    if (leftHalf[i] <= rightHalf[j]) {
+      result[k++] = leftHalf[i++]
+    } else {
+      result[k++] = rightHalf[j++]
+    }
+  }
+
+  while (i < leftHalf.length) {
+    result[k++] = leftHalf[i++]  
+  }
+
+  while (j < rightHalf.length) {
+    result[k++] = rightHalf[j++]
+  }
+
+  return result
 }
