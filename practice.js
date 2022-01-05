731046bb827f5ffa408a78c11067c2f57fef9d8d
Dmitrii An <an.dmitriy93@gmail.com>
2022-01-05 11:33:59 -0600

    Merge two linked lists

--- practice.js
+++ practice.js
@@ -51,5 +51,22 @@ class LinkedList {
 }
 
 function mergeLinkedLists(headOne, headTwo) {
-	
+	let p1 = headOne
+  let p2 = headTwo
+  let prev = null;
+
+  while (p1 && p2) {
+    if (p1.value < p2.value) {
+      prev = p1;
+      p1 = p1.next
+    } else {
+      if (prev !== null) prev.next = p2
+      prev = p2;
+      p2 = p2.next;
+      prev.next = p1
+    }
+  }
+  if (p1 === null) prev.next = p2;
+  
+  return headOne.value < headTwo.value ? headOne : headTwo
 }
