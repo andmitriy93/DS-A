86d1793f0059ac61c516c3e742b30746db3b6d1d
Dmitrii An <an.dmitriy93@gmail.com>
2022-01-05 11:37:10 -0600

    Reverse linked list

--- practice.js
+++ practice.js
@@ -43,30 +43,47 @@
 
 
 
-class LinkedList {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// class LinkedList {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 
-function mergeLinkedLists(headOne, headTwo) {
-	let p1 = headOne
-  let p2 = headTwo
-  let prev = null;
+// function mergeLinkedLists(headOne, headTwo) {
+// 	let p1 = headOne
+//   let p2 = headTwo
+//   let prev = null;
 
-  while (p1 && p2) {
-    if (p1.value < p2.value) {
-      prev = p1;
-      p1 = p1.next
-    } else {
-      if (prev !== null) prev.next = p2
-      prev = p2;
-      p2 = p2.next;
-      prev.next = p1
-    }
+//   while (p1 && p2) {
+//     if (p1.value < p2.value) {
+//       prev = p1;
+//       p1 = p1.next
+//     } else {
+//       if (prev !== null) prev.next = p2
+//       prev = p2;
+//       p2 = p2.next;
+//       prev.next = p1
+//     }
+//   }
+//   if (p1 === null) prev.next = p2;
+
+//   return headOne.value < headTwo.value ? headOne : headTwo
+// }
+
+
+// Reverse linked list 
+
+function reverseLinkedList(head) {
+  let currentNode = head;
+  let previous = null;
+
+  while (currentNode) {
+    let tmp = currentNode.next;
+    currentNode.next = previous;
+    previous = currentNode
+    currentNode = tmp;
   }
-  if (p1 === null) prev.next = p2;
-  
-  return headOne.value < headTwo.value ? headOne : headTwo
+
+  return previous;
 }
