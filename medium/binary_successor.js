08b113a8730bd8d3c22b5fcfbd8083a0feff8f97
Dmitrii An <an.dmitriy93@gmail.com>
2022-01-13 15:47:57 -0600

    binary successor naive solution

--- medium/binary_successor.js
+++ medium/binary_successor.js
@@ -31,3 +31,34 @@
 
 */
 
+class BinaryTryy {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
+}
+
+function findSuccessor(tree, node) {
+  const inOrderTraversalOrder = getInOrderTraversalOrder(tree);
+
+  for (let idx = 0; idx < inOrderTraversalOrder.length; idx++) {
+    const currentNode = inOrderTraversalOrder[idx];
+    if (currentNode !== node) continue;
+
+    if (idx === inOrderTraversalOrder.length - 1) return null;
+
+    return inOrderTraversalOrder[idx + 1]
+  }
+}
+
+function getInOrderTraversalOrder(node, order=[]) {
+  if (node === null) return order;
+
+  getInOrderTraversalOrder(node.left, order);
+  order.push(node);
+  getInOrderTraversalOrder(node.right, order);
+
+  return order;
+}
